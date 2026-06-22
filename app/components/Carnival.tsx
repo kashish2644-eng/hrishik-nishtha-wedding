619184d1b16fe8d8fddeed8a9933d314edc00122
Kashish Jain <kashish2644@gmail.com>
2026-06-22 15:35:05 +0530

    Rename Myra component to Carnival

--- app/components/Carnival.tsx
+++ app/components/Carnival.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
-export default function Myra() {
+export default function Carnival() {
   return (
     <div className="flex justify-center py-4">
       <Image
         src="/images/carnival.png"
-        alt="Myra"
+        alt="Carnival"
         width={1080}
         height={1920}
         className="w-full max-w-[700px] h-auto"
