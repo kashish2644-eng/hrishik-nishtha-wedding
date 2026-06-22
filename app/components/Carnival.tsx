8312794b335697b406d8a52260e2f3f09233b400
Kashish Jain <kashish2644@gmail.com>
2026-06-22 15:32:58 +0530

    Add Carnival component with image display

---
+++ app/components/Carnival.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Myra() {
+  return (
+    <div className="flex justify-center py-4">
+      <Image
+        src="/images/carnival.png"
+        alt="Myra"
+        width={1080}
+        height={1920}
+        className="w-full max-w-[700px] h-auto"
+      />
+    </div>
+  );
+}
