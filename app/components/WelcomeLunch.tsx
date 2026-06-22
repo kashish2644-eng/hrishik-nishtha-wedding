d371e2cbfc408c7b56fe12831045f42d40ae893a
Kashish Jain <kashish2644@gmail.com>
2026-06-22 15:04:42 +0530

    Add WelcomeLunch component with image display

--- app/components/WelcomeLunch.tsx
+++ app/components/WelcomeLunch.tsx
@@ -1,1 +1,15 @@
+import Image from "next/image";
 
+export default function WelcomeLunch() {
+  return (
+    <div className="flex justify-center py-10">
+      <Image
+        src="/images/welcomelunch.png"
+        alt="Welcome Lunch"
+        width={1080}
+        height={1920}
+        className="w-full max-w-[700px] h-auto"
+      />
+    </div>
+  );
+}
