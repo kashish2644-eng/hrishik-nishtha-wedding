0b8f3a1311a6dbe6930604f28e0ad23c2c182161
Kashish Jain <kashish2644@gmail.com>
2026-06-23 10:04:44 +0530

    Refactor WelcomeLunch component layout
    Replaced Image component with a wrapper div and added particle divs for visual effects.

--- app/components/WelcomeLunch.tsx
+++ app/components/WelcomeLunch.tsx
@@ -3,13 +3,22 @@ import Image from "next/image";
 export default function WelcomeLunch() {
   return (
     <div className="flex justify-center py-4 welcome-reveal">
-      <Image
-        src="/images/welcomelunch.png"
-        alt="Welcome Lunch"
-        width={1080}
-        height={1920}
-        className="w-full max-w-[700px] h-auto"
-      />
+      <div className="relative w-full max-w-[700px]">
+
+        <Image
+          src="/images/welcomelunch.png"
+          alt="Welcome Lunch"
+          width={1080}
+          height={1920}
+          className="w-full h-auto"
+        />
+
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+
+      </div>
     </div>
   );
 }
